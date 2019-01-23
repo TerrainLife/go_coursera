7fd227ef6083d8294eb356718df4d78c54da0f6a
TerrainLife <embedded.life@yandex.ru>
2019-01-23 22:11:18 +0300

    Async workers. Test passed

--- hw2_signer/signer.go
+++ hw2_signer/signer.go
@@ -4,29 +4,68 @@ import (
 	"fmt"
 	"sort"
 	"strconv"
+	"sync"
 	"time"
 )
 
-// сюда писать код
+func crc32Async(wg *sync.WaitGroup, data string, out chan<- string) {
+	defer wg.Done()
+	out <- DataSignerCrc32(data)
+}
+
+func singleHashWorker(wg *sync.WaitGroup, data1 string, data2 string, out chan<- interface{}) {
+	defer wg.Done()
+	resChan := make(chan string, 2)
+	wg.Add(1)
+	go crc32Async(wg, data1, resChan)
+	time.Sleep(time.Millisecond)
+	wg.Add(1)
+	go crc32Async(wg, data2, resChan)
+	res := <-resChan + "~" + <-resChan
+	out <- res
+	fmt.Println("SingleHash result ", res)
+}
+
+func multiHashWorker(wg *sync.WaitGroup, input []string, out chan<- interface{}) {
+	defer wg.Done()
+	resChan := make(chan string, 6)
+	for _, inp := range input {
+		wg.Add(1)
+		go crc32Async(wg, inp, resChan)
+		time.Sleep(time.Millisecond)
+	}
+
+	var res string
+	for idx := 0; idx < len(input); idx++ {
+		res += <-resChan
+	}
+	out <- res
+	fmt.Println("MultiHash result ", res)
+}
+
 func SingleHash(in, out chan interface{}) {
+	wg := &sync.WaitGroup{}
 	for val := range in {
 		strval := strconv.Itoa(val.(int))
-		res := DataSignerCrc32(strval) + "~" + DataSignerCrc32(DataSignerMd5(strval))
-		fmt.Println("SingleHash result ", res)
-		out <- res
+		md5 := DataSignerMd5(strval)
+		wg.Add(1)
+		go singleHashWorker(wg, strval, md5, out)
 	}
+	wg.Wait()
 }
 
 func MultiHash(in, out chan interface{}) {
 	hashvals := []int{0, 1, 2, 3, 4, 5}
+	wg := &sync.WaitGroup{}
 	for val := range in {
-		var res string
+		var inStr []string
 		for _, hv := range hashvals {
-			res += DataSignerCrc32(strconv.Itoa(hv) + val.(string))
+			inStr = append(inStr, strconv.Itoa(hv)+val.(string))
 		}
-		fmt.Println("MultiHash result ", res)
-		out <- res
+		wg.Add(1)
+		go multiHashWorker(wg, inStr, out)
 	}
+	wg.Wait()
 }
 
 func CombineResults(in, out chan interface{}) {
@@ -42,24 +81,28 @@ func CombineResults(in, out chan interface{}) {
 			res += "_"
 		}
 	}
-	fmt.Println("CombineResults ", res)
 	out <- res
+	fmt.Println("CombineResults ", res)
 }
+func jobWorker(wg *sync.WaitGroup, jobFn job, in, out chan interface{}) {
+	defer wg.Done()
+	defer close(out)
 
+	jobFn(in, out)
+}
 func ExecutePipeline(jobs ...job) {
 	chans := make([]chan interface{}, len(jobs)+1)
 	for idx := 0; idx < len(jobs)+1; idx++ {
 		chans[idx] = make(chan interface{}, 1)
 	}
 
+	wg := &sync.WaitGroup{}
 	for idx, worker := range jobs {
-		go worker(chans[idx], chans[idx+1])
+		wg.Add(1)
+		go jobWorker(wg, worker, chans[idx], chans[idx+1])
 	}
-	time.Sleep(3 * time.Second)
-	for _, channel := range chans {
-		close(channel)
-		time.Sleep(time.Second)
-	}
+
+	wg.Wait()
 }
 
 func main() {
